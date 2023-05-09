5ad84c701b907efd4b14ba636cce6260ef526f88
Douglas Berkley <douglasmberkley@gmail.com>
2023-05-09 10:58:43 +0900

    morning livecode

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,56 @@
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    # This empty instance is for the form builder
+    @restaurant = Restaurant.new
+  end
+
+  # this action DOES NOT have a view
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      # display the form page again
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    # this is for the form builder
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  # this action DOES NOT have a view
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    if @restaurant.update(restaurant_params)
+      redirect_to restaurant_path(@restaurant)
+    else
+      # display the form page again
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  # this action DOES NOT have a view
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    # status is for turbo
+    redirect_to restaurants_path, status: :see_other
+  end
+
+  private
+
+  # strong params -> white listing the info coming from the form
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :rating)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,31 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # verb '/path', to: 'controller#action', as: :prefix
+  # prefix ONLY refers to the path (not the http verb)
+  # restaurants_path
+  # restaurants_path, method: :post
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # CRUD
+  # Create
+  # we need a page for the form
+  get '/restaurants/new', to: 'restaurants#new', as: :new_restaurant
+  post '/restaurants', to: 'restaurants#create', as: :restaurants
+
+  # Read all
+  get '/restaurants', to: 'restaurants#index'
+
+  # Read one (need the id)
+  # add :id inside of our params
+  get '/restaurants/:id', to: 'restaurants#show', as: :restaurant
+
+  # Update
+  get '/restaurants/:id/edit', to: 'restaurants#edit', as: :edit_restaurant
+  patch '/restaurants/:id', to: 'restaurants#update'
+
+  # Destroy
+  delete '/restaurants/:id', to: 'restaurants#destroy'
+
+  # To build all 7 routes (CANT USE TODAY)
+  # resources :restaurants
 end
+# get '/books', to: 'books#index', as: :books
+# get '/games', to: 'games#index', as: :games
